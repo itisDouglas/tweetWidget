5f4e4014ad129f2736dc4d07edeabc930212d73a
Douglas Cueva <32880920+itisDouglas@users.noreply.github.com>
2019-10-28 20:16:57 -0400

    Add files via upload

---
+++ tweetWidget.py
@@ -0,0 +1,46 @@
+#A simple widget for sending tweets to my twitter without all the overhead in the app itself
+from tkinter import *
+import twitter
+
+api = twitter.Api(#API secret keys here)
+
+class Application(Frame):
+    def __init__(self, master):
+        #making the frame
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def get_input(self):
+        input_Val = self.text.get("1.0", "end-1c")
+        return str(input_Val)
+    
+    def create_widgets(self):
+        #creating widgets
+        #need a label, text box, button
+        self.lbl = Label(self, text = 'Tweet!')
+        self.lbl.grid_configure()
+        #this is my text box
+        self.text = Text(self, width = 25, height =5, wrap = WORD)
+        self.text.grid_configure(column=3)
+        #this is my button
+        self.btn = Button(self, text='Tweet!', command = lambda: self.tweet(self.get_input()))
+        self.btn.grid_configure(column=3)
+
+    
+
+    def tweet(self, texto):
+        #this method will be used to send a tweet
+        #it's activated when the button gets pressed
+        #how do i grab the text from the text box?
+        #establish a tweet parameter
+        #inputVal = self.text.get("1.0", "end-1c")
+        api.PostUpdate(status = str(texto))
+        
+    
+    
+root = Tk()
+root.title("Tweet widget")
+root.geometry('300x150')
+app = Application(root)
+root.mainloop()
