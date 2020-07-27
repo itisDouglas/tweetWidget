50cbaaf90228a038ead64f47aff1b26420b036b6
Douglas Cueva <dougcueva@gmail.com>
2020-07-27 05:53:15 -0400

    added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+
+    This config.py file is to declare your access and consumer keys from twitter.
+
+    Supply them after receiving this information from twitter.
+
+"""
+CONSUMER_KEY=''
+CONSUMER_SECRET=''
+ACCESS_TOKEN_KEY=''
+ACCESS_TOKEN_SECRET=''
--- tweetWidget.py
+++ tweetWidget.py
@@ -1,9 +1,21 @@
-#A simple widget for sending tweets to my twitter without all the overhead in the app itself
 from tkinter import *
 import twitter
-import keys
+import config
 
-api = twitter.Api(consumer_key= keys.CONSUMER_KEY, consumer_secret=keys.CONSUMER_SECRET, access_token_key=keys.ACCESS_TOKEN_KEY,access_token_secret=keys.ACCESS_TOKEN_SECRET)
+"""
+
+This tweet widget app is a simple gui that allows you to tweet without overhead of the website.
+
+The first thing that needs to be done is set your access and consumer key information in the
+config.py file. Afterwards, the api variable will hold all the information for you.
+
+This application sets the gui using tkinter.
+
+The tweet() takes an argument 'text'. This text values for this argument is supplied by the anonymous function in create_widgets(). Buttons in tkinter have a command value that can be supplied. In this app, this particular value is captured, sent to the tweet() and converted into a string. That string value is sent as a status updated (or tweet) to your twitter timeline.
+
+"""
+
+api = twitter.Api(consumer_key= config.CONSUMER_KEY, consumer_secret=config.CONSUMER_SECRET, access_token_key=config.ACCESS_TOKEN_KEY,access_token_secret=config.ACCESS_TOKEN_SECRET)
 
 class Application(Frame):
     def __init__(self, master):
@@ -30,13 +42,9 @@ class Application(Frame):
 
     
 
-    def tweet(self, texto):
+    def tweet(self, text):
         #this method will be used to send a tweet
-        #it's activated when the button gets pressed
-        #how do i grab the text from the text box?
-        #establish a tweet parameter
-        #inputVal = self.text.get("1.0", "end-1c")
-        api.PostUpdate(status = str(texto))
+        api.PostUpdate(status = str(text))
         
     
     
